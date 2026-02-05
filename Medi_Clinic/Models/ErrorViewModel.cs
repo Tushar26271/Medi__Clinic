dcaf07899371631ba924ab06ae8f7a0d6eba5018
TUSHARJHA <TUSHARJHA@IBM-832T8F4>
2026-02-05 16:40:28 +0530

    Add project files.

---
+++ Medi_Clinic/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Medi_Clinic.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
